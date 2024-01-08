90775d6043816c76c1ec4758289ed12c8c0b591a
Dmitry Afanasyev <Balshbox@gmail.com>
2024-01-08 03:26:42 +0300

    add more tests

--- bot_microservice/tests/integration/bot/test_bot_api.py
+++ bot_microservice/tests/integration/bot/test_bot_api.py
@@ -75,6 +75,38 @@ async def test_change_chatgpt_model_priority(
     assert upd_model2.priority == priority
 
 
+@pytest.mark.parametrize(
+    "bot_api_key_header",
+    [
+        pytest.param({"BOT-API-KEY": ""}, id="empty key in header"),
+        pytest.param({"BOT-API-KEY": "Hello World"}, id="incorrect token"),
+        pytest.param({"Hello-World": "lasdkj3-wer-weqwe_34"}, id="correct token but wrong api key"),
+        pytest.param({}, id="no api key header"),
+    ],
+)
+async def test_cant_change_chatgpt_model_priority_with_wrong_api_key(
+    dbsession: Session,
+    rest_client: AsyncClient,
+    faker: Faker,
+    test_settings: AppSettings,
+    bot_api_key_header: dict[str, str],
+) -> None:
+    ChatGptModelFactory(priority=0)
+    model2 = ChatGptModelFactory(priority=1)
+    priority = faker.random_int(min=2, max=7)
+    user = UserFactory(username=test_settings.SUPERUSER)
+    AccessTokenFactory(user_id=user.id, token="lasdkj3-wer-weqwe_34")  # noqa: S106
+    response = await rest_client.put(
+        url=f"/api/chatgpt/models/{model2.id}/priority",
+        json={"priority": priority},
+        headers=bot_api_key_header,
+    )
+    assert response.status_code == 403
+
+    changed_model = dbsession.query(ChatGptModels).filter_by(id=model2.id).one()
+    assert changed_model.priority == model2.priority
+
+
 async def test_reset_chatgpt_models_priority(
     dbsession: Session,
     rest_client: AsyncClient,
@@ -99,6 +131,37 @@ async def test_reset_chatgpt_models_priority(
 
     for model in models:
         assert model.priority == 0
+
+
+@pytest.mark.parametrize(
+    "bot_api_key_header",
+    [
+        pytest.param({"BOT-API-KEY": ""}, id="empty key in header"),
+        pytest.param({"BOT-API-KEY": "Hello World"}, id="incorrect token"),
+    ],
+)
+async def test_cant_reset_chatgpt_models_priority_with_wrong_api_key(
+    dbsession: Session, rest_client: AsyncClient, test_settings: AppSettings, bot_api_key_header: dict[str, str]
+) -> None:
+    chat_gpt_models = ChatGptModelFactory.create_batch(size=2)
+    model_with_highest_priority = ChatGptModelFactory(priority=42)
+
+    priorities = sorted([model.priority for model in chat_gpt_models] + [model_with_highest_priority.priority])
+
+    user = UserFactory(username=test_settings.SUPERUSER)
+    AccessTokenFactory(user_id=user.id)
+
+    response = await rest_client.put(
+        url="/api/chatgpt/models/priority/reset",
+        headers=bot_api_key_header,
+    )
+    assert response.status_code == 403
+
+    models = dbsession.query(ChatGptModels).all()
+
+    changed_priorities = sorted([model.priority for model in models])
+
+    assert changed_priorities == priorities
 
 
 async def test_create_new_chatgpt_model(
@@ -140,6 +203,37 @@ async def test_create_new_chatgpt_model(
         "model": model_name,
         "priority": model_priority,
     }
+
+
+async def test_cant_create_new_chatgpt_model_with_wrong_api_key(
+    dbsession: Session,
+    rest_client: AsyncClient,
+    faker: Faker,
+    test_settings: AppSettings,
+) -> None:
+    ChatGptModelFactory.create_batch(size=2)
+    ChatGptModelFactory(priority=42)
+
+    user = UserFactory(username=test_settings.SUPERUSER)
+    AccessTokenFactory(user_id=user.id)
+
+    model_name = "new-gpt-model"
+    model_priority = faker.random_int(min=1, max=5)
+
+    models = dbsession.query(ChatGptModels).all()
+    assert len(models) == 3
+
+    response = await rest_client.post(
+        url="/api/chatgpt/models",
+        json={
+            "model": model_name,
+            "priority": model_priority,
+        },
+    )
+    assert response.status_code == 403
+
+    models = dbsession.query(ChatGptModels).all()
+    assert len(models) == 3
 
 
 async def test_add_existing_chatgpt_model(
@@ -197,3 +291,27 @@ async def test_delete_chatgpt_model(
     assert len(models) == 2
 
     assert model not in models
+
+
+async def test_cant_delete_chatgpt_model_with_wrong_api_key(
+    dbsession: Session,
+    rest_client: AsyncClient,
+    test_settings: AppSettings,
+) -> None:
+    ChatGptModelFactory.create_batch(size=2)
+    model = ChatGptModelFactory(priority=42)
+
+    user = UserFactory(username=test_settings.SUPERUSER)
+    access_token = AccessTokenFactory(user_id=user.id)
+
+    models = dbsession.query(ChatGptModels).all()
+    assert len(models) == 3
+
+    response = await rest_client.delete(
+        url=f"/api/chatgpt/models/{model.id}",
+        headers={"ROOT-ACCESS": access_token.token},
+    )
+    assert response.status_code == 403
+
+    models = dbsession.query(ChatGptModels).all()
+    assert len(models) == 3
